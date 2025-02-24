d8718f0eb9a534a680b6e9964fb55a4c7459bc70
wsevolves <165700713+wsevolves@users.noreply.github.com>
2025-02-24 22:37:06 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "http://127.0.0.1:5500"],
+      origin: ["http://localhost:3000", "https://wsevolves.github.io/Harvey"],
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
